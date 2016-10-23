7a2b9d2011ba4380796e942e9f1126bc8caeb538
JLee <paperkatt@gmail.com>
2016-10-22 21:36:28 -0700

    add event activity

---
+++ app/src/main/java/com/apockestafe/team19/Event.java
@@ -0,0 +1,21 @@
+package com.apockestafe.team19;
+
+/**
+ * Created by JLee on 10/22/16.
+ */
+public class Event {
+
+    private String title, date, time, location, description;
+
+    public Event(String title, String date, String time, String location, String description ) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.description = description;
+    }
+
+
+
+}
+
--- app/src/main/java/com/apockestafe/team19/EventActivity.java
+++ app/src/main/java/com/apockestafe/team19/EventActivity.java
@@ -2,12 +2,40 @@ package com.apockestafe.team19;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class EventActivity extends AppCompatActivity {
+
+    private EditText title, date, time, location, description;
+    private Button createButton, cancelButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+        title = (EditText)findViewById(R.id.title);
+        date = (EditText)findViewById(R.id.date);
+        time = (EditText)findViewById(R.id.time);
+        location = (EditText)findViewById(R.id.location);
+        description = (EditText)findViewById(R.id.description);
+        createButton = (Button)findViewById(R.id.button_create);
+        cancelButton = (Button)findViewById(R.id.button);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String returner =
+            }
+        });
     }
 }
