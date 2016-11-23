c9fa7a00dc015493d7fce0445d5d31f36927e69a
JLee <paperkatt@gmail.com>
2016-11-22 20:12:55 -0800

    Destroyer Revision:
    Last commit before attempted merge with Nick.   Has Firebase event counter and better login handling
    
    -Justin Lee
    -Jordan
    -Rammy

--- app/src/main/java/com/apockestafe/team19/Event.java
+++ app/src/main/java/com/apockestafe/team19/Event.java
@@ -1,19 +1,158 @@
 package com.apockestafe.team19;
 
-/**
- * Created by JLee on 10/22/16.
- */
-public class Event {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import java.util.ArrayList;
+import java.util.Observable;
 
-    private String title, date, time, location, description, rideLocation[];
+import android.util.Log;
 
-    public Event(String title, String date, String time, String location, String description, String rideLocation[] ) {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.Observable;
+
+
+public class Event extends Observable {
+
+    private String title, date, time, location, description;
+    //private ArrayList<ArrayList<String>> rideLocation;
+    private String[] rideLocation;
+    private final DatabaseReference ref;
+    public boolean deleted;
+    public long counter;
+
+    public Event(String title, String date, String time, String location,
+                 String description, String[] rideLocation) {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //String key = ServerValue.TIMESTAMP.toString();
+        // String key;
+        setCounter();
         this.title = title;
         this.date = date;
         this.time = time;
         this.location = location;
         this.description = description;
         this.rideLocation = rideLocation;
+
+
+        ref = database.getReference("TEAM19/events/" + String.valueOf(counter));
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                changeData(dataSnapshot);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                deleted = true;
+                notifyObservers();
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void changeData (DataSnapshot dataSnapshot) {
+        String key = dataSnapshot.getKey();
+        if(key.equals("title")) {
+            title = dataSnapshot.getValue(String.class);
+        }
+        if(key.equals("time")) {
+            time = dataSnapshot.getValue(String.class);
+        }
+        if(key.equals("date")) {
+            date = dataSnapshot.getValue(String.class);
+        }
+        if(key.equals("location")) {
+            location = dataSnapshot.getValue(String.class);
+        }
+        if(key.equals("description")) {
+            description = dataSnapshot.getValue(String.class);
+        }
+        if(key.equals("rideLocation")) {
+            //rideLocation = dataSnapshot.getValue(String[].class);
+        }
+    }
+
+    public void add() {
+        ref.setValue(this);
+
+    }
+
+    public void remove() {
+        ref.setValue(null);
+        deleted = true;
+        notifyObservers();
+    }
+
+    public void changeTitle (String title) {
+        if(deleted) {
+            return;
+        }
+        this.title = title;
+        ref.child("title").setValue(title);
+    }
+
+    public void changeTime (String time) {
+        if(deleted) {
+            return;
+        }
+        this.time = time;
+        ref.child("time").setValue(time);
+    }
+
+    public void changeDate (String date) {
+        if(deleted) {
+            return;
+        }
+        this.date = date;
+        ref.child("date").setValue(date);
+    }
+
+    public void changeLocation (String location) {
+        if(deleted) {
+            return;
+        }
+        this.location = location;
+        ref.child("location").setValue(location);
+    }
+
+    public void changeDescription (String description) {
+        if(deleted) {
+            return;
+        }
+        this.description = description;
+        ref.child("description").setValue(description);
+    }
+
+    public void changeRideLocation (String[] rideLocation) {
+        if(deleted) {
+            return;
+        }
+        this.rideLocation = rideLocation;
+        ref.child("rideLocation").setValue(rideLocation);
     }
 
     public String getTitle() {
@@ -38,6 +177,40 @@ public class Event {
 
     public String[] getRideLocation() { return rideLocation; }
 
+    public void addRideLocation(String[] rd) {
+
+        //rideLocation.add(rd);
+    }
+
+    public boolean equals (Object comparable) {return false;} //implement if need comparator
+
+    public String toString() { //implement for event display in listview?
+        return null;
+    }
+
+    public void setCounter() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref2 = database.getReference("TEAM19");
+        ref2.child("counter").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() == null) {
+                    counter = 0;
+                }
+                else{
+                    counter = (long)dataSnapshot.getValue();
+                    counter++;
+                    ref2.child("counter").setValue(counter);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 
 }
 
--- app/src/main/java/com/apockestafe/team19/EventInfo.java
+++ app/src/main/java/com/apockestafe/team19/EventInfo.java
@@ -5,17 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.AppInviteContent;
+import com.facebook.share.widget.AppInviteDialog;
 
 
 public class EventInfo extends AppCompatActivity {
 
-    private Button mapButton, listRideButton;
+    private Button mapButton, listRideButton, backButton, inviteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
-
         mapButton = (Button)findViewById(R.id.MapButton);
         mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +35,34 @@ public class EventInfo extends AppCompatActivity {
 
             }
         });
+
+        backButton = (Button)findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(EventInfo.this, LoginActivity.class));
+                finish();
+            }
+        });
+        inviteButton = (Button)findViewById(R.id.inviteButton);
+        inviteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonHandler();
+            }
+        });
+    }
+
+    public void buttonHandler() {
+        String appLinkUrl, previewImageUrl;
+        appLinkUrl = "www.google.com";
+        previewImageUrl = "www.google.com";
+        if(AppInviteDialog.canShow()) {
+            AppInviteContent content = new AppInviteContent.Builder()
+                    .setApplinkUrl(appLinkUrl)
+                    .setPreviewImageUrl(previewImageUrl)
+                    .build();
+            AppInviteDialog.show(this,content);
+        }
     }
 }
