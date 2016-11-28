459f631f75f6afc7841f0e6f610ecab42cc627f2
Nicholas Allaire <nallaire@ucsd.edu>
2016-11-28 05:41:28 -0800

    Testing

--- app/src/androidTest/java/com/apockestafe/team19/MainActivityTest.java
+++ app/src/androidTest/java/com/apockestafe/team19/MainActivityTest.java
@@ -1,10 +1,8 @@
 package com.apockestafe.team19;
 
-import android.app.Application;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.ApplicationTestCase;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,21 +15,27 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Created by nsallaire on 11/28/16.
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
-    }
-
+public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
+    public void ChangeviewWithAddEvent() {
         onView(withId(R.id.button1)).perform(click());
         onView(withId(R.id.title)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void LaunchScreen() {
+        onView(withId(R.id.button1)).perform(click());
+        onView(withId(R.id.title)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void ChangeviewWithSettingsButton() {
+        onView(withId(R.id.settingsButton)).perform(click());
+        onView(withId(R.id.firstNameEditText)).check(matches(isDisplayed()));
     }
 }
